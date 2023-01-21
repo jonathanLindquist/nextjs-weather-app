e1d966a76b55a7a13c66f3973beb117f39d8160b
Jonathan Lindquist <jonathan.lindquist@protonmail.com>
2023-01-21 09:46:47 -0600

    applying prettier across project

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,8 +7,8 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <>
-      <h1>Hello Next World</h1>
-    </>
+      <>
+        <h1>Hello Next World</h1>
+      </>
   )
 }
